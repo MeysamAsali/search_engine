a39df94d17ade07189c27d69afc1f3be8386a9dc
unknown <mazloomi2002@gmail.com>
2023-12-23 12:00:47 +0330

    part5

---
+++ TF-IDF_Counter.py
@@ -0,0 +1,18 @@
+import json
+import numpy as np
+
+
+tf_idf_lst = []
+
+
+for i in range(50000):
+    with open(f'WordsTF_IDF\doc_{i}.json', 'r') as json_file:
+            
+        tf_idf_txt = json.load(json_file)
+        tf_idf_sorted_txt = sorted(tf_idf_txt.items(), key=lambda x:x[3], reverse=True)
+        tf_idf_lst.append(tf_idf_sorted_txt[:10])
+
+
+
+my_array = np.array(tf_idf_lst)
+
