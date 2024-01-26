a90b8f73d22585f18056a785c3da0a6fde0dfc2c
unknown <mazloomi2002@gmail.com>
2024-01-26 22:30:53 +0330

    functional p

--- TF-IDF_Counter.py
+++ TF-IDF_Counter.py
@@ -1,18 +1,20 @@
 import json
 import numpy as np
 
+def calculate_tfidfs():
 
-tf_idf_lst = []
-
-
-for i in range(50001):
-    with open(f'WordsTF_IDF\doc_{i}.json', 'r') as json_file:
-            
-        tf_idf_txt = json.load(json_file)
-        tf_idf_sorted_txt = sorted(tf_idf_txt.items(), key=lambda x:x[3], reverse=True)
-        tf_idf_lst.append(tf_idf_sorted_txt[:10])
+    tf_idf_lst = []
 
 
+    for i in range(50001):
+        with open(f'WordsTF_IDF\doc_{i}.json', 'r') as json_file:
+                
+            tf_idf_txt = json.load(json_file)
+            tf_idf_sorted_txt = sorted(tf_idf_txt.items(), key=lambda x:x[3], reverse=True)
+            tf_idf_lst.append(tf_idf_sorted_txt[:10])
 
-my_array = np.array(tf_idf_lst)
 
+
+    my_array = np.array(tf_idf_lst)
+
+    return my_array
--- TF_Counter.py
+++ TF_Counter.py
@@ -1,17 +1,19 @@
 import json
 import numpy as np
 
+def calculate_tfs():
 
-tf_lst = []
-
-
-for i in range(50001):
-    with open(f'WordsTF\doc_{i}.json', 'r') as json_file:
-            
-        tf_txt = json.load(json_file)
-        tf_sorted_txt = sorted(tf_txt.items(), key=lambda x:x[1], reverse=True)
-        tf_lst.append(tf_sorted_txt[:10])
+    tf_lst = []
 
 
-my_array = np.array(tf_lst)
-print(my_array[20064])
+    for i in range(50001):
+        with open(f'WordsTF\doc_{i}.json', 'r') as json_file:
+                
+            tf_txt = json.load(json_file)
+            tf_sorted_txt = sorted(tf_txt.items(), key=lambda x:x[1], reverse=True)
+            tf_lst.append(tf_sorted_txt[:10])
+
+
+    my_array = np.array(tf_lst)
+    
+    return my_array
